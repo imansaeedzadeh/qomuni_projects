0d287abd8e604e0b6664e1d987e7b5007daf6aff
unknown <saeedzadeh.iman@gmail.com>
2024-07-17 14:40:40 +0330

    Initialized our Backend and Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  const hello = 1;
+  return (
+    <>
+      <div className="App">hello{hello}</div>
+      <div className="App">hello{hello}</div>
+    </>
+  );
+};
+
+export default App;
